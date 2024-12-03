c3f862e51d22e9e0d57767267dee23b03b189de2
Anderson Vieira <asvieira.dev@gmail.com>
2024-12-03 11:18:48 -0300

    exibe receita dinamicamente de acordo com paramentros da rota

--- js/main.js
+++ js/main.js
@@ -1,15 +1,54 @@
 import { receitas } from "../data/receitas.js";
 
 const gridReceitas = document.getElementById("grid-receitas");
+const listaIngredientes = document.getElementById("receita-individual-ingredientes")
+const listaPreparo = document.getElementById("receita-individual-preparo")
+const receitaIndividualGaleria = document.getElementById("receita-individual-galeria")
+
 
 const carregarReceitas = () => {
     receitas.forEach(receita => {
+        const linkReceita = document.createElement('a')
+        linkReceita.href = `cadastrado.html?id=${receita.id}`
         const imgReceita = document.createElement('img');
-        imgReceita.src = receita.imagem;
+        imgReceita.src = receita.imagem[0];
         imgReceita.alt = receita.nome;
-
-        gridReceitas.appendChild(imgReceita);
+        imgReceita.id = receita.id
+        linkReceita.appendChild(imgReceita)
+        gridReceitas.appendChild(linkReceita);
     });
 };
 
-window.onload = carregarReceitas;
+const carregarReceitaInividual = () => {
+    const params = new URLSearchParams(window.location.search);
+    const id = parseInt(params.get("id"))
+
+    const receita = receitas.find(receita => receita.id === id);  
+
+    receita.imagem.forEach(src => {
+      const galeriaImg = document.createElement('img');
+      galeriaImg.src = src;
+      galeriaImg.alt = receita.nome;
+      receitaIndividualGaleria.appendChild(galeriaImg);
+    });
+
+    receita.ingredientes.forEach(ingrediente => {
+      const item = document.createElement('li');
+      item.textContent = ingrediente;
+      listaIngredientes.appendChild(item);
+    });
+
+    receita.preparo.forEach(preparo => {
+      const item = document.createElement('li');
+      item.textContent = preparo;
+      listaPreparo.appendChild(item);
+    });
+}
+
+window.onload = () => {
+  if(gridReceitas) {
+    carregarReceitas();
+  } else if (listaIngredientes && listaPreparo && receitaIndividualGaleria) {
+    carregarReceitaInividual()
+  }
+}
